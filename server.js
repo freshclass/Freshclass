0bdf75858fb255bd5564f31a7297d281fc374d84
Glitch (declass) <none>
2020-08-27 19:31:56 +0000

    hey

--- server.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var app = express();
-app.use(express.static(__dirname));
-app.get("*", function(req, res) {
-  res.sendFile(__dirname + "/pages/404.html");
-});
-app.listen(8080);
