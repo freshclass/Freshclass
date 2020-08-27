fe0ed29c5f04a04ee0fe8ba79130cf8798603756
Glitch (declass) <none>
2020-08-27 20:51:21 +0000

    yay

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
-function about() {
-  history.pushState({}, document.title, "#about");
+function scrollto(e) {
+  history.pushState({}, document.title, "#" + e);
   window.scroll({
-    top: document.getElementById("about").getBoundingClientRect().top - 90,
+    top: document.getElementById(e).getBoundingClientRect().top - 90,
     behavior: "smooth"
   });
 }
 function copy() {
   var e = document.createElement("textarea");
   e.value =
-    '<link rel="stylesheet" href="https://declass.glitch.me/1.min.css">';
+    '<link rel="stylesheet" href="https://declass.netlify.app/css/declass.min.css">';
   document.body.appendChild(e);
   e.select();
   document.execCommand("copy");
   document.body.removeChild(e);
+}
+if (location.hash === "#about") {
+  scrollTo(0, 0);
+  scrollto("about");
 }
