7642c1cdedb9e5aed07365daaeb07d11c54fe8b0
Glitch (declass) <none>
2020-08-30 14:49:17 +0000

    🚩🚜 Updated with Glitch

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
 function scrollto(e) {
   history.pushState({}, document.title, "#" + e);
-  if (e === "about") {
+  if (e === "about" && location.pathname === "/") {
     document.title = "Declass | About";
   }
-  scroll({
-    top: document.getElementById(e).getBoundingClientRect().top - 100,
-    behavior: "smooth"
-  });
+  if (
+    pageYOffset <
+    document.getElementById(e).getBoundingClientRect().top - 100
+  ) {
+    scroll({
+      top: document.getElementById(e).getBoundingClientRect().top - 100,
+      behavior: "smooth"
+    });
+  }
 }
 function copy(x) {
   var e = document.createElement("textarea");
@@ -24,12 +29,12 @@ function copy(x) {
   document.execCommand("copy");
   document.body.removeChild(e);
 }
-if (location.hash) {
-  scrollTo(0, 0);
-  onload = function() {
+scrollTo(0, 0);
+onload = function() {
+  if (location.hash) {
     scrollto(location.hash.replace("#", ""));
-  };
-}
+  }
+};
 onhashchange = function() {
   scrollto(location.hash.replace("#", ""));
 };
